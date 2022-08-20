c89f86b9e0358454dc3e8b4671150b9cc34fd7c3
Ankush Dubey <ankushdubey099@gmail.com>
2022-08-20 16:02:04 +0530

    Add files via upload

---
+++ Controllers/EmployeeApiDbController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiEmployee.Models;
+
+namespace WebApiEmployee.Controllers
+{
+    
+           
+    public class EmployeeApiDbController : ApiController
+    {
+        CrudOperationDBEmpEntities db = new CrudOperationDBEmpEntities();
+        [HttpGet]
+        public IHttpActionResult ActionModel()
+        {
+            List<CrudEmployeedb> list = db.CrudEmployeedbs.ToList();
+            return Ok(list);
+        }
+        [HttpGet]
+     public IHttpActionResult GetEmployeeMethod(int id)
+        {
+            var obj = db.CrudEmployeedbs.Where(Model=> Model.Id == id).FirstOrDefault();
+            return Ok(obj);
+        }
+
+        [HttpPost]
+        public IHttpActionResult EmployeeInsert(CrudEmployeedb crd)
+        {
+            db.CrudEmployeedbs.Add(crd);
+            db.SaveChanges();
+            return Ok();
+        }
+        [HttpPut]
+        public IHttpActionResult EmployeeUpdate(CrudEmployeedb crd)
+        {
+            
+            db.Entry(crd).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            //var emp = db.CrudEmployeedbs.Where(Model => Model.Id == crd.Id).FirstOrDefault();
+            //if (emp != null)
+            //{
+            //    emp.Id = crd.Id;
+            //    emp.Name = crd.Name;
+            //    emp.Aderess = crd.Aderess;
+            //    emp.Designation = crd.Designation;
+            //    emp.Salary = crd.Salary;
+            //    db.SaveChanges();
+            //}
+            //else
+            //{
+            //    return NotFound();
+            //}
+            return Ok();
+        }
+        [HttpDelete]
+        public IHttpActionResult EmployeeDelete(int id)
+        {
+            var obj = db.CrudEmployeedbs.Where(Model => Model.Id == id).FirstOrDefault();
+            db.Entry(obj).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/EmployeeMvcController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApiEmployee.Models;
+
+namespace WebApiEmployee.Controllers
+{
+    public class EmployeeMvcController : Controller
+    {
+        // GET: EmployeeMvc
+        HttpClient client = new HttpClient();
+        public ActionResult Index()
+        {
+            List<CrudEmployeedb> lists = new List<CrudEmployeedb>();
+            client.BaseAddress = new Uri("https://localhost:44393/api/EmployeeApiDb");
+            var responce = client.GetAsync("EmployeeApiDb");
+            responce.Wait();
+
+            var test = responce.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<CrudEmployeedb>>();
+                display.Wait();
+                lists = display.Result;
+            }
+            return View(lists);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(CrudEmployeedb create)
+        {
+
+            client.BaseAddress = new Uri("https://localhost:44393/api/EmployeeApiDb");
+            var responce = client.PostAsJsonAsync<CrudEmployeedb>("EmployeeApiDb", create);
+            responce.Wait();
+
+            var test = responce.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+
+        }
+        public ActionResult Details(int id)
+        {
+            CrudEmployeedb crd = null;
+            client.BaseAddress = new Uri("https://localhost:44393/api/EmployeeApiDb");
+            var responce = client.GetAsync("EmployeeApiDb?id=" + id.ToString());
+            responce.Wait();
+
+            var test = responce.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var dispaly = test.Content.ReadAsAsync<CrudEmployeedb>();
+                dispaly.Wait();
+                crd = dispaly.Result;
+            }
+
+            return View(crd);
+        }
+        public ActionResult Edit(int id)
+        {
+            CrudEmployeedb crd = null;
+            client.BaseAddress = new Uri("https://localhost:44393/api/EmployeeApiDb");
+            var responce = client.GetAsync("EmployeeApiDb?id=" + id.ToString());
+            responce.Wait();
+
+            var test = responce.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var dispaly = test.Content.ReadAsAsync<CrudEmployeedb>();
+                dispaly.Wait();
+                crd = dispaly.Result;
+            }
+
+            return View(crd);
+        }
+        [HttpPost]
+        public ActionResult Edit(CrudEmployeedb crd)
+        {
+            client.BaseAddress = new Uri("https://localhost:44393/api/EmployeeApiDb");
+            var responce = client.PutAsJsonAsync<CrudEmployeedb>("EmployeeApiDb", crd);
+            responce.Wait();
+
+            var test = responce.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+        }
+        public ActionResult Delete(int id)
+        {
+            CrudEmployeedb crd = null;
+            client.BaseAddress = new Uri("https://localhost:44393/api/EmployeeApiDb");
+            var responce = client.GetAsync("EmployeeApiDb?id=" + id.ToString());
+            responce.Wait();
+
+            var test = responce.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var dispaly = test.Content.ReadAsAsync<CrudEmployeedb>();
+                dispaly.Wait();
+                crd = dispaly.Result;
+            }
+
+            return View(crd);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            client.BaseAddress = new Uri("https://localhost:44393/api/EmployeeApiDb");
+            var responce = client.DeleteAsync("EmployeeApiDb/" + id.ToString());
+            responce.Wait();
+
+            var test = responce.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Delete");
+        }
+
+    }
+}
